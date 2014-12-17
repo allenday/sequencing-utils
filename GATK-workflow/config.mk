46f281d4f61176c53fc38d46363059f9f912d796
Allen Day <aday@mapr.com>
2014-12-17 23:53:15 +0000

    bringing over changes from parallelism branch to address chrM problems

--- GATK-workflow/config.mk
+++ GATK-workflow/config.mk
@@ -1,8 +1,8 @@
 SAM_HEADER=../NA12878.new_header.sam
-ALIGN_BASE=../hg19/hg19.fa
+ALIGN_BASE=../synthetic
+ALIGN_GENOME=synthetic.fa
 ANNOTATION=../vcf
 TOOLS=../
-REFERENCE=../synthetic/synthetic.fa
 # 2 GB per thread is required for Data Threads
 DATA_THREADS=12
 # Memory is shared for CPU threads
